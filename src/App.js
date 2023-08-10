7fd9e3ae10195021165a75fee945252500c68957
Matias Gonzalez <mng_epson@hotmail.com>
2023-08-10 16:43:35 -0300

    refactor: [organizing files and create product details folder]

--- src/App.js
+++ src/App.js
@@ -1,32 +1,9 @@
-import { useEffect, useState } from 'react';
-import axios from 'axios';
 import './App.css';
-import {ProductCard} from './components/ProductCard'
-
+import {Products} from './components/Products/Products'
 function App() {
-  const [products, setProducts] = useState([]);
-
-  useEffect(()=>{
-    axios.get('https://fakestoreapi.com/products')
-    .then((result)=>{
-      setProducts(result.data)
-    })
-  }, [])
-
-  return (
-    <div className="App">
-     <h1>Lista de productos</h1>
-     {products && products.map((product)=>{
-      return(
-        <ProductCard 
-        title={product.title} 
-        price={product.price}
-        image={product.image}
-        />
-      )
-     })}
-    </div>
-  );
+  return(
+    <Products/>
+  )
 }
 
 export default App;
---
+++ src/components/ProductDetails/ProductDetail.jsx
@@ -0,0 +1,5 @@
+export const ProductDetail = (title)=>{
+  return(
+    <h1>Informacion del Producto</h1>
+  )
+}
