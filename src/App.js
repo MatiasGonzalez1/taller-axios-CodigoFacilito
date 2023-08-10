15daae2d596332ec193745e88e1d42a290950ac6
Matias Gonzalez <mng_epson@hotmail.com>
2023-08-10 15:48:06 -0300

    feat: [add call to api]

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 import './App.css';
 import {ProductCard} from './components/ProductCard'
 
 function App() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(()=>{
+    axios.get('https://fakestoreapi.com/products')
+    .then((result)=>{
+      setProducts(result.data)
+    })
+  }, [])
+
   return (
     <div className="App">
      <h1>Lista de productos</h1>
-    <ProductCard title={'Producto de Prueba'} price={'12'}/>
+     {products && products.map((product)=>{
+      return(
+        <ProductCard title={product.title} price={product.price}/>
+      )
+     })}
     </div>
   );
 }
